f95f588e0e8b7d60e461bf796086a1e77f159626
Tom Forbes <tom@tomforb.es>
2018-10-08 02:18:24 +0100

    Improve tests, add django path validation

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,11 @@
 set -e
 IFS=$'\n\t'
 
+if [ -z "$(ls -A ./django/)" ]; then
+   echo "Incorrect DJANGO_PATH specified."
+   exit 1
+fi
+
 pip install -r tests/requirements/py3.txt
 if [ -n "$EXTRA_REQUIREMENTS" ]; then
     pip install -r tests/requirements/${EXTRA_REQUIREMENTS}
